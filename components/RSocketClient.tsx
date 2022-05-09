4ac6f232db8f6f9b2bf72ebc5347705b444c6663
Kellen Miller <kellen.miller12@gmail.com>
2022-05-09 19:29:06 -0400

    updated rsocket connector
    Signed-off-by: Kellen Miller <kellen.miller12@gmail.com>

--- components/RSocketClient.tsx
+++ components/RSocketClient.tsx
@@ -1,43 +1,49 @@
 import { ConnectorConfig, Payload, RSocketConnector } from "rsocket-core";
 import { WebsocketClientTransport } from "rsocket-websocket-client";
 
-function client(options: ConnectorConfig): RSocketConnector {
-  return new RSocketConnector({
-    setup: {
-      keepAlive: 1000000,
-      lifetime: 100000,
-      dataMimeType: "application/cbor",
-      metadataMimeType: "message/x.rsocket.routing.v0",
-    },
-    transport: new WebsocketClientTransport({
-      url: "ws://localhost:6565/rsocket",
-    }),
-  });
+const defaultSetup = {
+  keepAlive: 1000000,
+  lifetime: 100000,
+  dataMimeType: "application/cbor",
+  metadataMimeType: "message/x.rsocket.routing.v0",
+};
 
-  async function makeRequest(client: RSocketConnector, data: JSON) {
-    return new Promise(async (resolve, reject) => {
-      const connection = await client.connect();
-      connection.requestResponse(
-        {
-          data: Buffer.from(JSON.stringify(data)),
+function connectorConfig(url: string): ConnectorConfig {
+  return {
+    setup: defaultSetup,
+    transport: new WebsocketClientTransport({
+      url: url,
+    }),
+  };
+}
+
+function connector(options: ConnectorConfig): RSocketConnector {
+  return new RSocketConnector(options);
+}
+
+async function makeRequest(rSocketConnector: RSocketConnector, data: JSON) {
+  return new Promise(async (resolve, reject) => {
+    const connection = await rSocketConnector.connect();
+    connection.requestResponse(
+      {
+        data: Buffer.from(JSON.stringify(data)),
+      },
+      {
+        onExtension(): void {
+          // TODO document why this method 'onExtension' is empty
         },
-        {
-          onExtension(): void {
-            // TODO document why this method 'onExtension' is empty
-          },
-          onError: (e: Error) => reject(e),
-          onNext: (payload: Payload, isComplete: boolean) => {
-            console.log(
-              `payload[data: ${payload.data}; metadata: ${payload.metadata}]|${isComplete}`
-            );
-            resolve(payload);
-          },
-          onComplete: () => {
-            console.log("emailed");
-            resolve("emailed");
-          },
-        }
-      );
-    });
-  }
+        onError: (e: Error) => reject(e),
+        onNext: (payload: Payload, isComplete: boolean) => {
+          console.log(
+            `payload[data: ${payload.data}; metadata: ${payload.metadata}]|${isComplete}`
+          );
+          resolve(payload);
+        },
+        onComplete: () => {
+          console.log("emailed");
+          resolve("emailed");
+        },
+      }
+    );
+  });
 }

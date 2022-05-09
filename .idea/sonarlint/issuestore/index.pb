ffda14e150f98e965ef47b13b4ef531ef587bf47
Kellen Miller <kellen.miller12@gmail.com>
2022-05-09 16:38:59 -0400

    added gitignore
    Signed-off-by: Kellen Miller <kellen.miller12@gmail.com>

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+B
+components/Map.tsx,1/a/1ade5371a032baeecf3606b166314557c18d00c3
--- components/RSocketClient.tsx
+++ components/RSocketClient.tsx
@@ -1,43 +1,43 @@
-import {ConnectorConfig, Payload, RSocketConnector} from "rsocket-core";
-import {WebsocketClientTransport} from "rsocket-websocket-client";
+import { ConnectorConfig, Payload, RSocketConnector } from "rsocket-core";
+import { WebsocketClientTransport } from "rsocket-websocket-client";
 
 function client(options: ConnectorConfig): RSocketConnector {
-    return new RSocketConnector({
-        setup: {
-            keepAlive: 1000000,
-            lifetime: 100000,
-            dataMimeType: 'application/cbor',
-            metadataMimeType: 'message/x.rsocket.routing.v0',
-        },
-        transport: new WebsocketClientTransport({
-            url: 'ws://localhost:6565/rsocket'
-        }),
-    })
+  return new RSocketConnector({
+    setup: {
+      keepAlive: 1000000,
+      lifetime: 100000,
+      dataMimeType: "application/cbor",
+      metadataMimeType: "message/x.rsocket.routing.v0",
+    },
+    transport: new WebsocketClientTransport({
+      url: "ws://localhost:6565/rsocket",
+    }),
+  });
 
-    async function makeRequest(client: RSocketConnector, data: JSON) {
-        return new Promise(async (resolve, reject) => {
-            const connection = await client.connect()
-            connection.requestResponse(
-                {
-                    data: Buffer.from(JSON.stringify(data)),
-                },
-                {
-                    onExtension(): void {
-                        // TODO document why this method 'onExtension' is empty
-                    },
-                    onError: (e: Error) => reject(e),
-                    onNext: (payload: Payload, isComplete: boolean) => {
-                        console.log(
-                            `payload[data: ${payload.data}; metadata: ${payload.metadata}]|${isComplete}`
-                        );
-                        resolve(payload);
-                    },
-                    onComplete: () => {
-                        console.log("emailed")
-                        resolve("emailed");
-                    }
-                }
-            )
+  async function makeRequest(client: RSocketConnector, data: JSON) {
+    return new Promise(async (resolve, reject) => {
+      const connection = await client.connect();
+      connection.requestResponse(
+        {
+          data: Buffer.from(JSON.stringify(data)),
+        },
+        {
+          onExtension(): void {
+            // TODO document why this method 'onExtension' is empty
+          },
+          onError: (e: Error) => reject(e),
+          onNext: (payload: Payload, isComplete: boolean) => {
+            console.log(
+              `payload[data: ${payload.data}; metadata: ${payload.metadata}]|${isComplete}`
+            );
+            resolve(payload);
+          },
+          onComplete: () => {
+            console.log("emailed");
+            resolve("emailed");
+          },
         }
-    );
+      );
+    });
+  }
 }
